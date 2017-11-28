a856ef71ed125d386243a2b58a4dc34f551a46ed
YKFlash <yong.keong@live.com>
2017-11-28 13:59:42 +0800

    Merge origin/master

--- src/java/servlet/companyListViewServlet.java
+++ src/java/servlet/companyListViewServlet.java
@@ -5,13 +5,20 @@
  */
 package servlet;
 
+import beans.Application;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import utils.CoordinatorFunctionUtils;
+import utils.DBUtils;
+import utils.MyUtils;
 
 /**
  *
@@ -29,10 +36,32 @@ public class companyListViewServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+  
+    public companyListViewServlet() {
+        super();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
+           throws ServletException, IOException {
+        Connection conn = MyUtils.getStoredConnection(request);
+
+        String errorString = null;
+        List<Application> list = null;
+        try {
+            list = CoordinatorFunctionUtils.queryCompany(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errorString = e.getMessage();
+        }
+        // Store info in request attribute, before forward to views
+        request.setAttribute("errorString", errorString);
+        request.setAttribute("companyList", list);
+
+       // Forward to /WEB-INF/views/productListView.jsp
+        RequestDispatcher dispatcher = request.getServletContext()
+                .getRequestDispatcher("/WEB-INF/views/companyListView.jsp");
+        dispatcher.forward(request, response);
     }
 
     /**
